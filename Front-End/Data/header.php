3d01a58c1987f000448305442684d9a3c330cf43
Tinayeshe Mugwagwa <tinayeshem@gmail.com>
2024-10-23 10:54:35 +0200

    first commit

---
+++ Front-End/Data/header.php
@@ -0,0 +1,18 @@
+
+
+
+<head>
+
+        <nav class="nav-wrapper">
+            
+            <div classname = "nav-links">
+                <a classname = "nav-links" href="">Favourites</a>
+                <a classname = "nav-links" href="">Videos</a>
+                <a  classname = "nav-links"href="">Albums</a>
+                <a href="">Trending</a>
+                <a href="">Charts</a>
+            </div>
+            <div classname = "Logo"><a href="">Vibes</a></div>
+
+        </nav>
+<head>
