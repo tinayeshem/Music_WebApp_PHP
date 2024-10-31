467cd3edcc196b22344353d06afc7ead2be86dc4
Tinayeshe Mugwagwa <tinayeshem@gmail.com>
2024-10-31 14:56:56 +0100

    first commit with nav-bar, footer and header file.

---
+++ Front-End/Data/nav.php
@@ -0,0 +1,46 @@
+
+
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     
+    <link rel="stylesheet" href="../styles/nav.css?v=<?php echo time(); ?>">
+ 
+      
+   
+    <title>Document</title>
+</head>
+
+<body>
+
+        
+
+                <div class = "nav-container">
+                    <div class = "Logo">
+                        <div><a href="">LOGO</a></div>
+                    </div>
+                    <div class = "links">
+                        
+                            <li href = "#">Home</li>
+                            <li href = "#">Trending</li>
+                            <li href = "#">Albums</li>
+                            <li href = "#">Videos</li>
+                            <li href = "#">Charts</li>
+                        
+
+                    </div>
+
+                    <div class = register>                        
+                            <li>Get Started</li> 
+                    </div>
+                </div>
+
+        
+
+
+    
+</body>
+
+
+</html>
